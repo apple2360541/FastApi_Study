435225eb4ca1c1e76d82b6b83c92a7fe516ba74b
apple2360541 <apple9302@126.com>
2020-07-20 23:51:19 +0800

    oauth2

--- routers/depends.py
+++ routers/depends.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Cookie, Header, HTTPException
 
 app = APIRouter()
 
@@ -33,3 +33,34 @@ def get_users(commons: CommonQueryParams = Depends(CommonQueryParams)):
     items = fake_items_db[commons.skip: commons.skip + commons.limit]
     response.update({"items": items})
     return response
+
+
+def query_extractor(q: str = None):
+    return q
+
+
+def query_or_cookie_extractor(q: str = Depends(query_extractor), last_query: str = Cookie(None)):
+    if not q:
+        return last_query
+    return q
+
+
+@app.get("/items/more_depend/")
+async def more_depend(query_or_default: str = Depends(query_or_cookie_extractor)):
+    return {"q_or_cookie": query_or_default}
+
+
+async def verify_token(x_token: str = Header(...)):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+
+async def verify_key(x_key: str = Header(...)):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
+
+
+@app.get("/items/read/", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def read_items():
+    return [{"item": "Foo"}, {"item": "Bar"}]
